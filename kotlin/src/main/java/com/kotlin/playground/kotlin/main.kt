d3c8a1a12cc0de7dc2ecb2d83a8e23da9c663654
raytang <demo@gmai.com>
2017-08-07 14:46:45 +0800

    spark engine added
    spark engine added

--- kotlin/src/main/java/com/kotlin/playground/kotlin/main.kt
+++ kotlin/src/main/java/com/kotlin/playground/kotlin/main.kt
@@ -1,14 +1,22 @@
 package com.kotlin.playground.kotlin
 
+import spark.ModelAndView
 import spark.Spark.*
+import spark.template.velocity.VelocityTemplateEngine
 import com.fasterxml.jackson.module.kotlin.*
 import com.kotlin.playground.kotlin.dao.DaoFactory
 import com.kotlin.playground.kotlin.model.*
 
 fun main(args: Array<String>) {
-    
+
     val mapper = jacksonObjectMapper().registerKotlinModule()
     port(12312)
     get("/") { _, _ -> "Hello" }
-    get("/Customers") { _, _ -> mapper.writeValueAsString(DaoFactory.CustomerDAO.findAll().first()) }
+    post("/Customers") { _, _ -> mapper.writeValueAsString(DaoFactory.CustomerDAO.findAll().first()) }
+    get("/Customers") { _, _ ->
+        {
+            val customers = DaoFactory.CustomerDAO.findAll();
+            VelocityTemplateEngine().render(ModelAndView(mapOf("customers" to customers), "velocity/table.vm"))
+        }
+    }
 }
