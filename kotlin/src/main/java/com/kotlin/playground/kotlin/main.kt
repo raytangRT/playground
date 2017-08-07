0ace1c6c9d6aeaada9d62f70ad71a12870b52399
raytang <demo@gmai.com>
2017-08-07 13:40:36 +0800

    Initial design
    Initial restful design

--- kotlin/src/main/java/com/kotlin/playground/kotlin/main.kt
+++ kotlin/src/main/java/com/kotlin/playground/kotlin/main.kt
@@ -1,8 +1,14 @@
 package com.kotlin.playground.kotlin
 
-import com.github.andrewoma.kwery.core.*
+import spark.Spark.*
+import com.fasterxml.jackson.module.kotlin.*
+import com.kotlin.playground.kotlin.dao.DaoFactory
 import com.kotlin.playground.kotlin.model.*
-import com.kotlin.playground.kotlin.dao.*
 
 fun main(args: Array<String>) {
+    
+    val mapper = jacksonObjectMapper().registerKotlinModule()
+    port(12312)
+    get("/") { _, _ -> "Hello" }
+    get("/Customers") { _, _ -> mapper.writeValueAsString(DaoFactory.CustomerDAO.findAll().first()) }
 }
