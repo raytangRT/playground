5c54fdfb68562ff88d54841e835c26582fdc6620
raytang <demo@gmai.com>
2017-10-10 15:46:24 +0800

    exclude node_moduels
    exclude node_moduels

---
+++ home/build/ViewModels/index.js
@@ -0,0 +1,13 @@
+define(["require", "exports", "knockout"], function (require, exports, ko) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var ViewModel = /** @class */ (function () {
+        function ViewModel(language, framework) {
+            this.language = ko.observable(language);
+            this.framework = ko.observable(framework);
+        }
+        return ViewModel;
+    }());
+    ko.applyBindings(new ViewModel("12", "13"));
+});
+//# sourceMappingURL=index.js.map
--- home/src/require-config.ts
+++ home/src/require-config.ts
@@ -1,6 +1,7 @@
 
 require.config({
     paths: {
-        "knockout" : "../node_modules/knockout/build/output/knockout-latest"
+        "knockout" : "../node_modules/knockout/build/output/knockout-latest", 
+        "jquery" : "../node_modules/jquery/dist/jquery"
     }
 })
